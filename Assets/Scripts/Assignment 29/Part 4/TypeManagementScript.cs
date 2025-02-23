e50959be64badc3fe9b982e5e8dc633e90f6ec00
LinaAbufaraha <linaabufarha1@gmail.com>
2025-02-23 22:33:11 +0200

    commit1

---
+++ Assets/Scripts/Assignment 29/Part 4/TypeManagementScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assignment29
+{
+    public class TypeManagementScript : MonoBehaviour
+    {
+        void Start()
+        {
+        
+        }
+
+        void Update()
+        {
+        
+        }
+    }
+}
